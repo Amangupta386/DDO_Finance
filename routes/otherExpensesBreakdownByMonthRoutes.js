a73c28c5e2cb7fc3e7795ff8d97f99501addbc49
rajat <rajat.chaurasia@walkingtree.tech>
2024-04-10 19:23:58 +0530

    AD-sd

--- routes/otherExpensesBreakdownByMonthRoutes.js
+++ routes/otherExpensesBreakdownByMonthRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const otherExpensesController = require('../controllers/database1/otherExpensesController');
 
 
-router.route('/')
-  // Get all expenses
-  .get(otherExpensesController.getAllExpenses)
-  // Create a new expense
-  .post(otherExpensesController.createExpense);
+// router.route('/')
+//   // Get all expenses
+//   .get(otherExpensesController.getAllExpenses)
+//   // Create a new expense
+//   .post(otherExpensesController.createExpense);
 
-router.route('/:id')
-  // Get an expense by ID
-  .get(otherExpensesController.getExpenseById)
-  // Update an expense by ID
-  .put(otherExpensesController.updateExpense)
-  // Delete an expense by ID
-  .delete(otherExpensesController.deleteExpense);
+// router.route('/:id')
+//   // Get an expense by ID
+//   .get(otherExpensesController.getExpenseById)
+//   // Update an expense by ID
+//   .put(otherExpensesController.updateExpense)
+//   // Delete an expense by ID
+//   .delete(otherExpensesController.deleteExpense);
 
 module.exports = router;
