b1689b16a14b072a5bbfc2604a86176ea1f6640d
rajat <rajat.chaurasia@walkingtree.tech>
2024-04-18 14:43:53 +0530

    routes

--- routes/forecastedCollectionBreakdownByMonthRoutes.js
+++ routes/forecastedCollectionBreakdownByMonthRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 const forecastedCollectionBreakdownByMonthController = require('../controllers/database1/forecastedCollectionBreakdownByMonthController');
 
+router.route('/f').get(forecastedCollectionBreakdownByMonthController.getDashboardForecastedCollection);
 router.route('/')
   .post(forecastedCollectionBreakdownByMonthController.createRecord)
   .get(forecastedCollectionBreakdownByMonthController.getAllRecords);
